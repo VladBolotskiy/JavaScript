7c05da9da98899b1cac2e8db8c9490e4072ee56d
VladBolotskiy <126961748+VladBolotskiy@users.noreply.github.com>
2025-02-23 22:57:39 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let answer = prompt("Hello! Whats up?");
-let answ = prompt("How are you doing?");
-let ans = prompt("How old are you??");
+let number = 10369;
+let result = number;
+let d1 =1;
+let d2 = 0;
+let d3 = 3;
+let d4 = 6;
+let d5 = 9;
 
-console.log(20)
-console.log(answer)
-console.log(answ)
-console.log(ans)
+console.log(`${d1} ${d2} ${d3} ${d4} ${d5}`)
