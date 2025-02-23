800a904ddeead0ca8ef9d201797213071f58f9ec
VladBolotskiy <126961748+VladBolotskiy@users.noreply.github.com>
2025-02-23 22:09:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+let answer = prompt("Hello! Whats up?");
+let answ = prompt("How are you doing?");
+let ans = prompt("How old are you??");
+
+console.log(20)
+console.log(answer)
+console.log(answ)
+console.log(ans)
